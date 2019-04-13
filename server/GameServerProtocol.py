7f2e351232a498e0e25b392ed0b157dc6856d8fc
tmek1244 <tmek1244@wp.pl>
2019-04-14 00:26:25 +0200

    v2

--- server/GameServerProtocol.py
+++ server/GameServerProtocol.py
@@ -1,14 +1,17 @@
-from twisted.internet import protocol
+import random
 
+from twisted.internet import reactor, protocol
+from twisted.protocols.basic import LineReceiver
+from twisted.internet.protocol import Factory
 
 class GameServerProtocol(protocol.Protocol):
-    def __init__(self, factory):
+    def __init__(self,factory):
         self.factory = factory
         self.is_playing = False
         self.opponent = None
-
+ 
     def connectionMade(self):
-        self.factory.players += 1
+        self.factory.ile += 1
 
     def dataReceived(self, data):
         data = data.decode('ascii')
@@ -17,7 +20,6 @@ class GameServerProtocol(protocol.Protocol):
             self.transport.write("leave".encode('ascii'))
             return
         if data == "reset" or data == "yes" or data == "no":
-            print("reset: ", data)
             data = data.encode('ascii')
             self.opponent.transport.write(data)
         else:
@@ -27,24 +29,40 @@ class GameServerProtocol(protocol.Protocol):
                 elif self not in self.factory.lookingForOpponent:
                     self.opponent = self.factory.lookingForOpponent.pop()
                     self.opponent.opponent = self
-                    self.transport.write("-1".encode('ascii'))
-                    self.opponent.transport.write("1".encode('ascii'))
+                    if random.randint(0, 1) == 1:
+                        self.transport.write("-1".encode('ascii'))
+                        self.opponent.transport.write("1".encode('ascii'))
+                    else:
+                        self.transport.write("1".encode('ascii'))
+                        self.opponent.transport.write("-1".encode('ascii'))
+                        
                     self.is_playing = True
                     self.opponent.is_playing = True
             else:
-                if self.opponent == "niema":
+                if self.opponent == "left":
                     self.transport.write("leave".encode('ascii'))
                 else:
-                    self.opponent.transport.write(data.encode('ascii'))
+                    self.opponent.transport.write(data.encode('ascii'))       
 
     def connectionLost(self, reason):
         try:
             self.factory.lookingForOpponent.remove(self)
-            print("wyszedl")
         except:
             try:
                 self.opponent.transport.write("leave".encode('ascii'))
-                self.opponent.opponent = "niema"
-                print("wyszedl1")
+                self.opponent.opponent = "left"
             except:
                 pass
+
+class GameServerFactory(Factory):
+    lookingForOpponent = []
+    ile = 0
+
+    def __init__(self):
+        print ("server is running...")
+    
+    def buildProtocol(self, addr):
+        return GameServerProtocol(self)
+
+reactor.listenTCP(8080, GameServerFactory())
+reactor.run()
